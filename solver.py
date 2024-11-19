2d19c49007827cdfc1a4b8603b5dbca5fa1bc7b7
Kjetil Iversen <kjetil.iversen@tv2.no>
2024-11-19 22:43:32 +0100

    Print number of unknowns as debug

--- solver.py
+++ solver.py
@@ -12,19 +12,28 @@ class Solver:
 
 def solve_for_single_missing(board:SudokuBoard):
     for idx, row in enumerate(board.get_rows()):
-        if row.count('-') == 1:
+        number_of_unknowns = row.count('-')
+        if number_of_unknowns > 1:
+            print(f"row {idx+1} has {number_of_unknowns} unknowns")
+        if number_of_unknowns == 1:
             for i in range(1, 10):
                 if str(i) not in row:
                     board.update_cell(idx+1, row.index('-')+1, i)
                     break
     for idx, column in enumerate(board.get_columns()):
+        number_of_unknowns = column.count('-')
+        if number_of_unknowns > 1:
+            print(f"column {idx+1} has {number_of_unknowns} unknowns")
         if column.count('-') == 1:
             for i in range(1, 10):
                 if str(i) not in column:
                     board.update_cell(column.index('-')+1, idx+1, i)
                     break
     for idx, box in enumerate(board.get_boxes()):
-        if box.count('-') == 1:
+        number_of_unknowns = box.count('-')
+        if number_of_unknowns > 1:
+            print(f"box {idx+1} has {number_of_unknowns} unknowns")
+        if number_of_unknowns == 1:
             for i in range(1, 10):
                 if str(i) not in box:
                     board.update_cell(box.index('-')+1, idx+1, i)
